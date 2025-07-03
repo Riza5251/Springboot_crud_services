5c52b215861d3bba93665af7e8e12592a0ba604a
Riza5251 <rizaqureshi5251@gmail.com>
2025-07-03 13:56:29 +0530

    commit

--- src/main/java/net/javaguides/Springboot/SpringbootCrudServicesApplication.java
+++ src/main/java/net/javaguides/Springboot/SpringbootCrudServicesApplication.java
@@ -1,11 +1,22 @@
 package net.javaguides.Springboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(title="SpringBott API Documentation",
+							   description=	"SpringBoot Rest API Documentation",
+								version = "v1.0",
+                                contact = @Contact(name = "Riza",email = "rizaqureshi5251@gmail.com",
+								url = "https://www.google.com"),
+                                license = @License(name = "Apache 2.0",
+												url="https://www.google.com")))
 public class SpringbootCrudServicesApplication {
 
 	@Bean
